de254fb77261d2b6931a623092ee55e6ec03ec07
AlexViu <viu.alex771@gmail.com>
2021-01-11 12:24:16 +0100

    login

---
+++ AgendaWebJPA/src/main/java/mx/com/gm/sga/domain/Usuario.java
@@ -0,0 +1,51 @@
+package mx.com.gm.sga.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name="usuarios")
+@NamedQueries({
+	@NamedQuery(name="Usuario.findAll", query="SELECT u FROM Usuario u"),
+	@NamedQuery(name="Usuario.findByUserAndPwd", query="SELECT u FROM Usuario u where u.usuario=?1 and u.password=?2")
+})
+
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idUsuario;
+
+	private String password;
+
+	private String usuario;
+
+	public Usuario() {
+	}
+
+	public int getIdUsuario() {
+		return this.idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+}
